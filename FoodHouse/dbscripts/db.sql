c0376c2366c5f6f3d388d56e1a40a6cf5e7a415e
Trần Quang Tri <tranquangtri96v2@gmail.com>
2017-12-09 00:21:12 +0700

    Add DB scripts
    Note:
    
     -  seed.sql: yet completed

---
+++ FoodHouse/dbscripts/db.sql
@@ -0,0 +1,128 @@
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`name` nvarchar(64) COLLATE utf8_bin NOT NULL,
+	`email` varchar(512) NOT NULL UNIQUE,
+    `username` varchar(64) NOT NULL UNIQUE,
+    `password` varchar(64) NOT NULL,
+    `city` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `province` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `address` nvarchar(128) COLLATE utf8_bin NOT NULL,   
+    `phone` varchar(16) NOT NULL, 
+    PRIMARY KEY(`id`)
+);
+CREATE INDEX `idx_username` ON `users`(`username`);
+CREATE INDEX `idx_email` ON `users`(`email`);
+
+CREATE TABLE IF NOT EXISTS `user_roles`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(45) NOT NULL,
+    `role` varchar(45) NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_username` FOREIGN KEY (`username`)
+    REFERENCES `users`(`username`)
+);
+
+CREATE TABLE IF NOT EXISTS `foods` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `price` double unsigned NOT NULL,
+    `price_promotion` double unsigned NOT NULL,
+    `kind_food` nvarchar(64),
+    `image` varchar(512) NOT NULL,
+    `description` nvarchar(1024) COLLATE utf8_bin NOT NULL,
+     PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `branches` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `address` nvarchar(128) COLLATE utf8_bin NOT NULL,
+    `province` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `city` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `phone` varchar(16) COLLATE utf8_bin NOT NULL,
+    `open_time` datetime NOT NULL,
+    `close_time` datetime NOT NULL,
+    `table_quantity` int unsigned NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders`(
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`customer_id` int unsigned NOT NULL,
+	`branch_id` int unsigned NOT NULL,
+	`customer_name` nvarchar(64) NOT NULL,
+	`customer_phone` varchar(16)  NOT NULL,
+	`customer_email` varchar(512)  NOT NULL,
+	`customer_address` nvarchar(128)  NOT NULL,
+	`customer_city` nvarchar(64) NOT NULL,
+	`customer_province` nvarchar(64) NOT NULL,
+	`order_detail_id` int unsigned NOT NULL,
+    `total_money` double unsigned NOT NULL,
+    `date_time` datetime NOT NULL, 
+    `status` nvarchar(64) COLLATE utf8_bin NOT NULL,
+    `note` nvarchar(1024) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_orders_users` FOREIGN KEY (`customer_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `fk_orders_branches` FOREIGN KEY (`branch_id`) REFERENCES `branches`(`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `order_details` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`order_id` int unsigned NOT NULL,
+    `food_id` int unsigned NOT NULL,
+    `price` double unsigned NOT NULL,
+    `quantity` int unsigned NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_order_details_foods` FOREIGN KEY(`food_id`)
+    REFERENCES `foods`(`id`),
+    CONSTRAINT `fk_order_details_orders` FOREIGN KEY(`order_id`)
+    REFERENCES `orders`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `branches_foods` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`branch_id` int unsigned NOT NULL,
+    `food_id` int unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`branch_id`, `food_id`),
+    CONSTRAINT `fk_branch_has_food_branch` FOREIGN KEY (`branch_id`)
+    REFERENCES `branches`(`id`),
+    CONSTRAINT `fk_branch_has_food_food` FOREIGN KEY (`food_id`)
+    REFERENCES `foods`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `costs_incurred` (
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`date` date NOT NULL,
+	`branch_id` int unsigned NOT NULL,
+    `cause` NVARCHAR(1024) COLLATE utf8_bin NOT NULL,
+    `cost` double unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_costs-incurred-month_branch` FOREIGN KEY (`branch_id`)
+    REFERENCES `branches`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `visit_statistics`(
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`user_id` int unsigned, /* null: guest */
+	`date_time` datetime NOT NULL,
+	`route` varchar(1024),
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `categories`(
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`name` nvarchar(1024) NOT NULL,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `categories_foods`(
+	`id` int unsigned NOT NULL AUTO_INCREMENT,
+	`cat_id` int unsigned NOT NULL,
+	`food_id` int unsigned NOT NULL,
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`cat_id`) REFERENCES `categories`(`id`),
+	FOREIGN KEY(`food_id`) REFERENCES `foods`(`id`)
+);
---
+++ FoodHouse/dbscripts/seed.sql
@@ -0,0 +1,92 @@
+/*INSERT INTO `food` (`id`, `name`, `price`, `price_promotion`, `kindOfFood`, `image`, `description`) VALUES
+
+-- Starters--
+(1, 'Blue Cheese Salad', 15, 13, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/blue-cheese-salad-super-salads.jpg', 'Special'),
+(2, 'Donec', 35, 30, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/donec.jpg', ''),
+(3, 'Ens Banting', 21, 15, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/ens-banting.jpg', ''),
+(4, 'Fusce', 17, 12, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/fusce.jpg', 'Special'),
+(5, 'Ipsum', 24, 21, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/ipsum.jpg', ''),
+(6, 'Kitchen Stuff ', 55, 43, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/kitchen-stuff-chorizo.jpg', ''),
+(7, 'Lorem', 42, 40, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/lorem.jpg', ''),
+(8, 'Nullam', 32, 30, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/Nullam.jpg', 'Special'),
+(9, 'Spier Kitchen Stuff', 44, 38, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/spier-kitchen-stuff.jpg', ''),
+(10, 'Tempura Batter ', 70, 60, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/tempura-batter-filipino-recipes.jpg', ''),
+(11, 'Tempura Green Beans', 32, 21, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/tempura-green-beans.jpg', ''),
+(12, 'Vivamus', 55, 44, 'Starters', 'http://localhost:8080/FoodHouse/images/food/starters/vivamus.jpg', 'Special'),
+-- //Starters--
+
+-- Main Course --
+(13, 'Bistecca Tonno', 15, 13, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/BisteccaTonno.jpg', ''),
+(14, 'Bourbon Glazed', 88, 65, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/bourbon-glazed-salmon.jpg', ''),
+(15, 'Chile Brined Ham', 65, 47, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/chile-brined-ham.jpg', ''),
+(16, 'Grigliata', 50, 45, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/Grigliata.jpg', ''),
+(17, 'Lamb Chops ', 78, 57, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/lamb-chops-rosemary-xlg.jpg', 'Special'),
+(18, 'Maple Sugar Ginger', 68, 65, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/maple-sugar-ginger-roast-pork.jpg', ''),
+(19, 'Pasta Soup', 87, 66, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/pasta-soup-good-housekeeping.jpg', ''),
+(20, 'Prime Rib Roast', 24, 20, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/prime-rib-roast.jpg', ''),
+(21, 'Roast Duck Citrus', 36, 24, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/roast-duck-citrus-xlg.jpg', 'Special'),
+(22, 'Salmone Ferri', 70, 50, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/SalmoneFerri.jpg', 'Special'),
+(23, 'Tagliata Carne', 36, 30, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/Tagliata-Carne.jpg', ''),
+(24, 'Zuppa Oceanica', 54, 50, 'Main Course', 'http://localhost:8080/FoodHouse/images/food/main course/ZuppaOceanica.jpg', ''),
+-- //Main Course --
+
+-- Salads --
+(25, 'Greek Quinoa', 36, 25, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Greek_Quinoa_Salad.jpg', ''),
+(26, 'Grilled Nectarine', 32, 30, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Grilled_Nectarine_Salad.jpg', 'Special'),
+(27, 'Lamb Kebabs', 66, 57, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Lamb_Kebabs.jpg', ''),
+(28, 'Madeleine Recipes', 50, 44, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Madeleine_Recipes_Feta_raisin_salad.jpg', ''),
+(29, 'Recipes Chicken', 33, 21, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Recipes_watermelon_salad.jpg', ''),
+(30, 'Sesame Crusted', 44, 41, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Sesame_Crusted_Salmon.jpg', 'Special'),
+(31, 'Verggie Nori Wraps', 21, 15, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/VERGGIE_NORI_WRAPS.jpg', ''),
+(32, 'Recipes Watermelon', 25, 21, 'Salads', 'http://localhost:8080/FoodHouse/images/food/salads/Recipes_watermelon_salad.jpg', ''),
+-- //Salads
+
+-- Desserts --
+(33, 'Pink Snail', 44, 40, 'Desserts', 'http://localhost:8080/FoodHouse/images/food/desserts/Pink Snail.jpeg', ''),
+(34, 'Birds milk', 31, 29, 'Desserts', 'http://localhost:8080/FoodHouse/images/food/desserts/Birds milk.jpeg', 'Special'),
+(35, 'Crumbs', 33, 20, 'Desserts', 'http://localhost:8080/FoodHouse/images/food/desserts/Crumbs.jpeg', 'Special'),
+(36, 'Niger Smile', 12, 10, 'Desserts', 'http://localhost:8080/FoodHouse/images/food/desserts/Niger Smile.jpeg', '');
+
+INSERT INTO `users`(`username`, `password`, `name`, `phone`) VALUES
+-- Customer --
+('customer', '123456', 'Ashe', '0989112153'),
+
+-- Manager --
+('manager', '123456', 'Blitzcrank', '0988391293'),
+
+-- Admin --
+('admin', '123456', 'Corki', '01639103218');
+
+INSERT INTO `user_roles`(`username`, `role`) VALUES
+('customer', 'ROLE_CUSTOMER'),
+('manager', 'ROLE_MANAGER'),
+('admin', 'ROLE_ADMIN');
+
+
+INSERT INTO `branch` (`id`, `name`, `address`, `province`, `image`, `phone`, `numOfTable`, `infoTable`) VALUES
+-- TP HCM --
+(1, 'Food Mart - Q.1', '59 Tran Hung Dao', 'TP HCM', 'http://localhost:8080/FoodHouse/images/branches/TP HCM Q1.jpg', '028112153', 15, ''),
+(2, 'Food Mart - Q.3', '124 Cao Thang', 'TP HCM', 'http://localhost:8080/FoodHouse/images/branches/TP HCM Q3.jpg', '028391293', 12, ''),
+(3, 'Food Mart - Q.7', '256 Nguyen Van Linh', 'TP HCM', 'http://localhost:8080/FoodHouse/images/branches/TP HCM Q7.jpg', '028195100', 18, ''),
+(4, 'Food Mart - Q.8', '159 Hung Phu', 'TP HCM', 'http://localhost:8080/FoodHouse/images/branches/TP HCM Q8.jpg', '0289103218', 20, ''),
+(5, 'Food Mart - Q. Thu Duc', '255 Vo Van Ngan', 'TP HCM', 'http://localhost:8080/FoodHouse/images/branches/TP HCM Thu duc.jpg', '028786241', 20, ''),
+-- //TP HCM --
+
+-- Ha Noi --
+(6, 'Food Mart - Q.Ba Dinh','33 Tran Phu', 'Ha Noi', 'http://localhost:8080/FoodHouse/images/branches/Ha noi-Ba dinh.jpg', '024112153', 15, ''),
+(7, 'Food Mart - Q.Dong Da','259 Ton Duc Thang', 'Ha Noi', 'http://localhost:8080/FoodHouse/images/branches/Ha Noi-Dong Da.jpg', '024391293', 12, ''),
+(8, 'Food Mart - Q.Hoan Kiem','11 Thai Ha', 'Ha Noi', 'http://localhost:8080/FoodHouse/images/branches/Ha noi-Hoan kiem.jpg', '024195100', 18, ''),
+(9, 'Food Mart - Q.Tay Ho','534 Vo Chi Cong', 'Ha Noi', 'http://localhost:8080/FoodHouse/images/branches/Ha noi-Tay Ho.jpg', '0249103218', 11, ''),
+-- //Ha Noi --
+
+-- Da Nang --
+(10, 'Food Mart - Nguyen Trai','20 Nguyen Trai', 'Da Nang', 'http://localhost:8080/FoodHouse/images/branches/Da Nang1.jpg', '0236112153', 12, ''),
+(11, 'Food Mart - An Duong Vuong','99 An Duong Vuong', 'Da Nang', 'http://localhost:8080/FoodHouse/images/branches/Da Nang2.jpg', '0236391293', 14, ''),
+(12, 'Food Mart - Nguyen Van Cu','987 Nguyen Van Cu', 'Da Nang', 'http://localhost:8080/FoodHouse/images/branches/Da nang3.jpg', '02369103218', 17, ''),
+(13, 'Food Mart - Cao Thang','578 Cao Thang', 'Da Nang', 'http://localhost:8080/FoodHouse/images/branches/Da Nang4.jpg', '02369103218', 17, ''),
+-- //Da Nang --
+
+-- Nha Trang --
+(14, 'Food Mart - Quang Trung','195 Quang Trung', 'Nha Trang', 'http://localhost:8080/FoodHouse/images/branches/Nha Trang - 1.jpg', '0258112153', 18, ''),
+(15, 'Food Mart - Ly Thuong Kiet','36 Ly Thuong Kiet', 'Nha Trang', 'http://localhost:8080/FoodHouse/images/branches/Nha Trang - 2.jpg', '0258391293', 12, '');
+*/
